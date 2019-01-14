f267215c35341712754fb7f2f5c2bd9c4d6e25c1
Toth-Gabor <upcgabor@gmail.com>
2019-01-14 13:00:40 +0100

    Kangaroo and KangarooGenerator

--- Kangaroo.java
+++ Kangaroo.java
@@ -1,9 +1,35 @@
 /**
  * Kangaroo
  */
-public class Kangaroo extends Figther {
+public class Kangaroo extends Fighter {
 
-    public Kangaroo(){
+    private int specialAttack;
+
+    public Kangaroo(String name, int health, int damageReduction, int attack){
         
+        super(name, health, damageReduction, attack);
+    }
+    public int bigKick(){
+        specialAttack = this.attack + 20;
+        if (this.damageReduction - 10 > 0) {
+            this.damageReduction -= 10;
+        }
+        return specialAttack;
+    }
+
+    public int tailStike(){
+        specialAttack = this.attack + 40;
+        if (this.damageReduction - 10 > 0) {
+            this.damageReduction -= 10;
+        }
+        return specialAttack;
+    }
+
+    public int incrediblePunch(){
+        specialAttack = this.attack + 50;
+        if (this.damageReduction - 10 > 0) {
+            this.damageReduction -= 10;
+        }
+        return specialAttack;
     }
 }
--- KangarooGenerator.java
+++ KangarooGenerator.java
@@ -1,7 +1,22 @@
+import java.util.Random;
+
 /**
  * KangarooGenerator
  */
 public class KangarooGenerator {
 
-    
+    private int kangarooOrder;
+
+    public Kangaroo createKangaroo(){
+        String[] names = {"Killer", "Józsi", "Joe", "Gizi", "Pakkó", "Robertó",
+                            "Jimmi", "Oszkár", "Skippy", "Immre a pusztító"};
+        int rnd = new Random().nextInt(names.length);
+        String name = names[rnd];
+        Random rnd2 = new Random();
+        int health = rnd2.nextInt((100 - 60) + 1) + 60; 
+        int damageReduction = rnd2.nextInt((10 - 5) + 1) + 5;
+        int attack = rnd2.nextInt((10 - 5) + 1) + 5; 
+        Kangaroo kangaroo = new Kangaroo(name, health, damageReduction, attack);
+        return kangaroo;
+    }
 }
