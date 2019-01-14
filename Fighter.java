20b75d09d30a68551d46d5650bbd4176f3666a79
levayb <levayboldizsar@gmail.com>
2019-01-14 10:20:22 +0100

    more class

--- Fighter.java
+++ Fighter.java
@@ -11,5 +11,17 @@ public class Fighter {
     public Fighter(String name, int health, int defense, int attack){
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public boolean isDead(){
+        if(this.health <= 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 
 }
