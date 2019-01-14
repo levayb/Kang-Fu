a0d4017e718f26803050657603e7ba1ddb9bb2c1
levayb <levayboldizsar@gmail.com>
2019-01-14 14:26:54 +0100

    PairGenerator/pairMaker

--- PairGenerator.java
+++ PairGenerator.java
@@ -8,10 +8,22 @@ public class PairGenerator {
     public PairGenerator() {
     }
 
-    public Object[][] pairMaker(Kangaroo[]) throws ValueException{
-        if (Kangaroo[].length % 2 != 0){
+    public Object[][] pairMaker(Object[] objects) throws ValueException{
+        if (objects.length % 2 != 0){
             throw new ValueException();
         }
-        Array[][] figthers = new Array[Kangaroo.length / 2][2];
+        Random random = new Random();
+        Array[][] pairs = new Array[objects.length / 2][2]; 
+        int maxObject = objects.length;
+        int index = 0;
+        while (maxObject > 2) {
+            for (int i = 0; i < 2; i = i++) {
+                int choice = random.nextInt(maxObject);
+                pairs[index][i] = objects[choice];
+                objects[choice] = objects[maxObject-1]; 
+                maxObject -= 1;
+            }
+            index ++;
+        }
     }
 }
