edc5ff2932b4cc9d048ee8d4ed789f40e5def0ea
Szűcs Áron <aron.szucs.miskolc@gmail.com>
2019-01-15 12:06:57 +0100

    logger merged; Main refactored for testing

--- Main.java
+++ Main.java
@@ -6,22 +6,34 @@ import java.util.*;
  */
 public class Main {
 
+    /*
+        Main entry point
+    */
     public static void main(String[] args) {
         battleTest();
+        kangarooGeneratorTest();
+        menuTest();
     }
+
+    /* 
+        Seperate testing methods to test individual functionalities
+        Feel free to create new test methods for your testing needs
+        and call them in main() 
+    */
     public static void battleTest() {
-        //Menu menu = new Menu();
+        // For testing the battle system
                                     
         Battle battle = new Battle();
         KangarooGenerator k = new KangarooGenerator();
         Fighter[] kangaroos = k.createKangaroos(32);
         battle.runRound(kangaroos);
-
-        // System.out.println(kangaroos[10].getName());
-        //battle.runRound(kangaroos);
     } 
-    public void kangarooGeneratorTest() {
-        
+    public static void kangarooGeneratorTest() {
+        // For testing Kangaroo generation
+        KangarooGenerator k = new KangarooGenerator();
+    }
+    public static void menuTest() {
+        // For menu system testing
     }
     
 
