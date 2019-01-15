937ab8190e11169cdd96bac8a02e0a44a4928493
Toth-Gabor <upcgabor@gmail.com>
2019-01-15 10:44:31 +0100

    new Menu.java

--- Main.java
+++ Main.java
@@ -32,13 +32,15 @@ public class Main {
         */
 
         // Testing sets
-        Battle battle = new Battle();
-        KangarooGenerator k = new KangarooGenerator();
-        Fighter[] kangaroos = k.createKangaroos(10);
-        
-        battle.runRound(kangaroos);
+                // Battle battle = new Battle();
+                // KangarooGenerator k = new KangarooGenerator();
+                // Fighter[] kangaroos = k.createKangaroos(10);
+                
+                // battle.runRound(kangaroos);
         // System.out.println(kangaroos[10].getName());
         // battle.runRound(kangaroos);
-
-    }
+        //idáig törölni
+        Menu menu = new Menu();
+                                     
+    } 
 }
