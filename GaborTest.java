ec24a0eff254b277206f5e1fe6724b2008b2a412
Toth-Gabor <upcgabor@gmail.com>
2019-01-17 16:47:17 +0100

    nem tudom

---
+++ GaborTest.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+
+
+
+/**
+ * Main
+ */
+public class GaborTest {
+
+    /*
+        Main entry point
+    */
+    public static void main(String[] args) {
+        Logger logger = new Logger();
+        logger.setLogging(false, true, true);
+        logger.setWaitingForUser(false, false, false);
+        // battleTest(logger);
+        // kangarooGeneratorTest(logger);
+        menuTest(logger);
+        // fileTester(logger);
+        testWriteKangaroosToFile();
+        
+    }
+
+    /* 
+        Seperate testing methods to test individual functionalities
+        Feel free to create new test methods for your testing needs
+        and call them in main() 
+    */
+    public static void battleTest(Logger logger) {
+        // For testing the battle system
+        Battle battle = new Battle(logger);
+        KangarooGenerator k = new KangarooGenerator();
+        Fighter[] kangaroos = k.createKangaroos(16);
+        battle.runTournament(kangaroos);
+        for (Fighter kangaroo : kangaroos) {
+            System.out.println(kangaroo.getStatistics().getPrintStr());
+        }
+    } 
+
+    public static void kangarooGeneratorTest(Logger logger) {
+        // For testing Kangaroo generation
+        KangarooGenerator k = new KangarooGenerator();
+    }
+
+    public static void menuTest(Logger logger) {
+        Menu menu = new Menu(logger);
+        menu.handleMenu();
+    }
+    
+    public static void fileTester(Logger logger) {
+        FileManager fileManager = new FileManager();
+        try {
+            String[][] s = fileManager.read("files/goat.csv");
+           
+            logger.tablePrinter.printTable(s);
+            fileManager.write("files/output", s);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    
+    }
+    public static void testWriteKangaroosToFile(){
+        FileManager fm = new FileManager();
+        KangarooGenerator kangarooGenerator = new KangarooGenerator(fm);
+        Kangaroo[] kangaroos = kangarooGenerator.createKangaroosFromFile("files/kangaroosStats.csv");
+        kangarooGenerator.writeKangaroosToFile(kangaroos);
+        //kangarooGenerator.createKangaroosFromFile();
+        //for (Kangaroo var : kangaroos) {
+        //    System.out.println( var.getId() + " " + var.getName() + " " + var.getHealth()+ " " + var.getDefense()+ " " + var.getattack());
+        //}
+    }
+}
